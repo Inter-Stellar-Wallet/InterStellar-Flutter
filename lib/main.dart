98459332b3dc1772dfd241e22c1b1a3cfd866a63
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-06-16 04:34:18 +0530

    Added Wallet UI and SDK Demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,21 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:intersteller_wallet/pages/intro_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:stellar_flutter_sdk/stellar_flutter_sdk.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  // ignore: unused_element
+  Future<void> _stellarSDKDemo() async {
+    final StellarSDK sdk = StellarSDK.TESTNET;
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  final StellarSDK sdk = StellarSDK.TESTNET;
-
-  Future<void> test() async {
     // String accountId =
     //     "GASYKQXV47TPTB6HKXWZNB6IRVPMTQ6M6B27IM5L2LYMNYBX2O53YJAL";
     // AccountResponse account = await sdk.accounts.account(accountId);
@@ -43,8 +24,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // create a completely new and unique pair of keys.
     KeyPair keyPair = KeyPair.random();
 
-    print("${keyPair.accountId}");
-    print("${keyPair.secretSeed}");
+    print(keyPair.accountId);
+    print(keyPair.secretSeed);
 
     String mnemonic = await Wallet.generate24WordsMnemonic();
     print(mnemonic);
@@ -58,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
     print("${keyPair1.accountId} : ${keyPair1.secretSeed}");
 
     bool funded = await FriendBot.fundTestAccount(keyPair.accountId);
-    print("funded: ${funded}");
+    print("funded: $funded");
 
     AccountResponse account = await sdk.accounts.account(keyPair.accountId);
 
@@ -80,42 +61,19 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     for (String key in account.data.keys) {
-      print("Data key: ${key} value: ${account.data[key]}");
+      print("Data key: $key value: ${account.data[key]}");
     }
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Interstellar Wallet',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: test,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const IntroPage(),
     );
   }
 }
