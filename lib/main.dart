e7aabb0794b784732851d4e254ceda7ed7388929
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-06-16 02:55:27 +0530

    Added Demo Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stellar_flutter_sdk/stellar_flutter_sdk.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,46 +8,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -56,53 +32,73 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final StellarSDK sdk = StellarSDK.TESTNET;
+
+  Future<void> test() async {
+    // String accountId =
+    //     "GASYKQXV47TPTB6HKXWZNB6IRVPMTQ6M6B27IM5L2LYMNYBX2O53YJAL";
+    // AccountResponse account = await sdk.accounts.account(accountId);
+    // print("sequence number: ${account.sequenceNumber}");
+
+    // create a completely new and unique pair of keys.
+    KeyPair keyPair = KeyPair.random();
+
+    print("${keyPair.accountId}");
+    print("${keyPair.secretSeed}");
+
+    String mnemonic = await Wallet.generate24WordsMnemonic();
+    print(mnemonic);
+
+    Wallet wallet = await Wallet.from(mnemonic);
+
+    KeyPair keyPair0 = await wallet.getKeyPair(index: 0);
+    print("${keyPair0.accountId} : ${keyPair0.secretSeed}");
+
+    KeyPair keyPair1 = await wallet.getKeyPair(index: 1);
+    print("${keyPair1.accountId} : ${keyPair1.secretSeed}");
+
+    bool funded = await FriendBot.fundTestAccount(keyPair.accountId);
+    print("funded: ${funded}");
+
+    AccountResponse account = await sdk.accounts.account(keyPair.accountId);
+
+    for (Balance balance in account.balances) {
+      switch (balance.assetType) {
+        case Asset.TYPE_NATIVE:
+          print("Balance: ${balance.balance} XLM");
+          break;
+        default:
+          print(
+              "Balance: ${balance.balance} ${balance.assetCode} Issuer: ${balance.assetIssuer}");
+      }
+    }
+
+    print("Sequence number: ${account.sequenceNumber}");
+
+    for (Signer signer in account.signers) {
+      print("Signer public key: ${signer.accountId}");
+    }
+
+    for (String key in account.data.keys) {
+      print("Data key: ${key} value: ${account.data[key]}");
+    }
+  }
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -116,10 +112,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: test,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
